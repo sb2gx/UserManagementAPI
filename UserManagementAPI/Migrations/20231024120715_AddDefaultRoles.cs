1363ed934aa2eccee3891181faa9848c21a6463f
Olujitan Olusegun <sb2gi@yahoo.com>
2023-10-24 13:15:13 +0100

    Add Default Roles Delete Controllers

---
+++ UserManagementAPI/Migrations/20231024120715_AddDefaultRoles.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserManagementAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDefaultRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"INSERT INTO [dbo].[Roles]([Name]) VALUES('Admin'),('Customer')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DELETE FROM [dbo].[Roles]");
+        }
+    }
+}
